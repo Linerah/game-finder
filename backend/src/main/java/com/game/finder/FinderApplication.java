0482c840f723ffeebe7e74f6ca11051747b2eb08
kevinlinera <kevin.linera@upr.edu>
2021-09-29 14:13:11 -0400

    first commit
    backend and frontend

---
+++ backend/src/main/java/com/game/finder/FinderApplication.java
@@ -0,0 +1,39 @@
+package com.game.finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class FinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinderApplication.class, args);
+	}
+
+	  /*	Cross-origin resource sharing (CORS) is a W3C specification
+		implemented by most browsers that allows you to specify in a
+		flexible way what kind of cross domain requests are authorized,
+		instead of using some less secured and less powerful hacks. */
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Authorization", "Origin","Accept", "X-Request-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
+}
---
+++ backend/src/main/java/com/game/finder/repositories/PlayerRepository.java
@@ -0,0 +1,19 @@
+package com.game.finder.repositories;
+
+import com.game.finder.domain.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+//<type of object, type of the primary key/id for the object>
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long>{
+
+    //We could run it without the @Query tag, but just leave it to be more specific.
+    //This is JPQL, not straight SQL
+    //It is a query method, so it can run its own, thanks to JPA ;)
+    @Query("SELECT s FROM Player s WHERE s.email = ?1")
+    Optional<Player> findPlayerByEmail(String email);
+}
